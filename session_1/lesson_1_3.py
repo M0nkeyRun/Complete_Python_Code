f9ab8a3efd426ea77dcf0456454d471d0abb6cc3
M0nkeyRun <fenil9311@gmail.com>
2025-06-29 11:11:08 +0100

    Add files via upload

---
+++ session_1/lesson_1_3.py
@@ -0,0 +1,9 @@
+# implicit type cast
+num = int(input("Enter a number: "))
+
+print(f"The cube of {num} is {num**3}")
+
+# explicit type cast
+
+num1 = input("\nEnter another number: ")
+print(int(num1) + int(num1))
